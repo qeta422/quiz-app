056c6ce8edcd32b8032f0044d21507b32d1c15a4
qeta422 <qetafetviashvili123@gmail.com>
2024-02-15 23:53:40 +0400

    html and styles, javaScript quiz data

---
+++ main.js
@@ -0,0 +1,45 @@
+const data = [
+    {
+        id: 1,
+        question: "Which of these fish is actually a fish?",
+        answers: [
+            { answer: "swordfish", isCorrect: true },
+            { answer: "jellyfish", isCorrect: false },
+            { answer: "starfish", isCorrect: false },
+            { answer: "crayfish", isCorrect: false },
+        ]
+    },
+
+    {
+        id: 2,
+        question: "A pickle is made from which vegetable?",
+        answers: [
+            { answer: "Potato", isCorrect: false },
+            { answer: "Carrot", isCorrect: false },
+            { answer: "Cucumber", isCorrect: true },
+            { answer: "Cabbage", isCorrect: false },
+        ]
+    },
+
+    {
+        id: 3,
+        question: "How many sides does a triangle have?",
+        answers: [
+            { answer: "1", isCorrect: false },
+            { answer: "3", isCorrect: true },
+            { answer: "4", isCorrect: false },
+            { answer: "2", isCorrect: false },
+        ]
+    },
+
+    {
+        id: 4,
+        question: "Who painted the Mona Lisa?",
+        answers: [
+            { answer: "Amedeo Modighliani", isCorrect: false },
+            { answer: "Vincent Van Gogh", isCorrect: false },
+            { answer: "Gustav Klimt", isCorrect: false },
+            { answer: "Leonardo Da Vinchi", isCorrect: true },
+        ]
+    },
+]
